c0d79a4eee66c7df98c15b432539932c5e7cd1d5
FELLIPE NEGHERBON PRIM <fellipe.prim@univille.br>
2026-06-10 15:11:39 -0300

    Estrutura inicial do projeto AtendeLab

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+//Atenção à porta do MySQL
+//Se o MySQL estiver configurado em outra porta, por exemplo 3307, será necessário
+//informar a porta na string de conexão.
+
+$host = 'localhost';
+$port = 3306; // Porta padrão do MySQL
+$dbname = 'atendelab';
+$user = 'root';
+$password = '';
+try {
+    $pdo = new PDO(
+    "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+    $user,
+    $password
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+}
