51b8249bb9c469afabd79e2b07586edae3a57f54
FELLIPE NEGHERBON PRIM <fellipe.prim@univille.br>
2026-06-22 21:21:55 -0300

    Patch: implementando o resto das sessoes

--- config/database.php
+++ config/database.php
@@ -1,21 +1,28 @@
 <?php
 
-//Atenção à porta do MySQL
-//Se o MySQL estiver configurado em outra porta, por exemplo 3307, será necessário
-//informar a porta na string de conexão.
+$host = '127.0.0.1';
+$porta = '3307'; 
+$banco = 'atendelab';
+$usuario = 'root';
+$senha = '';
 
-$host = 'localhost';
-$port = 3306; // Porta padrão do MySQL
-$dbname = 'atendelab';
-$user = 'root';
-$password = '';
 try {
     $pdo = new PDO(
-    "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-    $user,
-    $password
+        "mysql:host={$host};port={$porta};dbname={$banco};charset=utf8mb4",
+        $usuario,
+        $senha
     );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE, 
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE, 
+        PDO::FETCH_ASSOC
+    );
+
 } catch (PDOException $e) {
-    die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+    exit('Erro ao conectar com o banco de dados');
 }
